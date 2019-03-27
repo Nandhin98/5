e76c35d834805da74bbf75c53630084231549eb7
Nandhin98 <47237438+Nandhin98@users.noreply.github.com>
2019-03-27 16:10:04 +0530

    Create 5.java

---
+++ 5.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class KRoundingOfN
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Input : ");
+        long n=sc.nextInt();
+        long k=sc.nextInt();
+        
+        int i=1;
+        while(true)
+        {
+            long product=n*i;
+            if(product%(Math.pow(10,k))==0)
+            {
+                System.out.println("Output :\n"+product);
+                break;
+            }   
+            i+=1;
+        }
+    }
+}
